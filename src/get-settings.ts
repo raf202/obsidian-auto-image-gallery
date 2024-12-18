7304d703eb15a0f7dd2963b3fe3f3f635763670a
Sigma <raphadko@gmail.com>
2024-12-18 15:28:07 -0300

    Updates

--- src/get-settings.ts
+++ src/get-settings.ts
@@ -1,22 +1,22 @@
-import { normalizePath, parseYaml, Platform } from 'obsidian'
-import renderError from './render-error'
+import { normalizePath, parseYaml, Platform, App } from "obsidian";
+import renderError from "./render-error";
 
-const getSettings = (src: string, container: HTMLElement) => {
+const getSettings = (app: App, src: string, container: HTMLElement) => {
   // parse the settings from the code block
-  const settingsSrc: any = parseYaml(src)
+  const settingsSrc: any = parseYaml(src);
 
   // check for required settings
   if (settingsSrc === undefined) {
-    const error = 'Cannot parse YAML!'
-    renderError(container, error)
-    throw new Error(error)
+    const error = "Cannot parse YAML!";
+    renderError(container, error);
+    throw new Error(error);
   }
 
-  if (!settingsSrc.path) {
-    const error = 'Please specify a path!'
-    renderError(container, error)
-    throw new Error(error)
-  }
+  // if (!settingsSrc.path) {
+  //   const error = "Please specify a path!";
+  //   renderError(container, error);
+  //   throw new Error(error);
+  // }
 
   // store settings, normalize and set sensible defaults
   const settings = {
@@ -29,24 +29,32 @@ const getSettings = (src: string, container: HTMLElement) => {
     mobile: undefined as number,
     columns: undefined as number,
     height: undefined as number,
-  }
+  };
 
-  settings.path = normalizePath(settingsSrc.path)
-  settings.type = settingsSrc.type ?? 'horizontal'
-  settings.radius = settingsSrc.radius ?? 0
-  settings.gutter = settingsSrc.gutter ?? 8
-  settings.sortby = settingsSrc.sortby ?? 'ctime'
-  settings.sort = settingsSrc.sort ?? 'desc'
+  console.log("tessst");
+  console.log(app);
+  // const app = new App();
+  const notePath = app.workspace.getActiveFile().parent.path;
+  const noteName = app.workspace.getActiveFile().basename;
+
+  settings.path = normalizePath(
+    settingsSrc.path ?? `${notePath}/attachments/${noteName}`
+  );
+  settings.type = settingsSrc.type ?? "horizontal";
+  settings.radius = settingsSrc.radius ?? 0;
+  settings.gutter = settingsSrc.gutter ?? 8;
+  settings.sortby = settingsSrc.sortby ?? "ctime";
+  settings.sort = settingsSrc.sort ?? "desc";
 
   // settings for vertical mansory only
-  settings.mobile = settingsSrc.mobile ?? 1
-  if (Platform.isDesktop) settings.columns = settingsSrc.columns ?? 3
-  else settings.columns = settings.mobile
+  settings.mobile = settingsSrc.mobile ?? 1;
+  if (Platform.isDesktop) settings.columns = settingsSrc.columns ?? 3;
+  else settings.columns = settings.mobile;
 
   // settings for horizontal mansory only
-  settings.height = settingsSrc.height ?? 260
+  settings.height = settingsSrc.height ?? 260;
 
-  return settings
-}
+  return settings;
+};
 
-export default getSettings
+export default getSettings;
--- src/init.ts
+++ src/init.ts
@@ -1,16 +1,16 @@
-import { App, MarkdownRenderChild } from 'obsidian'
-import ImgGallery from './main'
-import getImagesList from './get-imgs-list'
-import getSettings from './get-settings'
-import buildHorizontal from './build-horizontal'
-import buildVertical from './build-vertical'
-import buildLightbox from './build-lightbox'
+import { App, MarkdownRenderChild } from "obsidian";
+import ImgGallery from "./main";
+import getImagesList from "./get-imgs-list";
+import getSettings from "./get-settings";
+import buildHorizontal from "./build-horizontal";
+import buildVertical from "./build-vertical";
+import buildLightbox from "./build-lightbox";
 
 export class imgGalleryInit extends MarkdownRenderChild {
-  private _gallery: HTMLElement = null
-  private _lightbox: any = null
-  private _settings: {[key: string]: any} = {}
-  private _imagesList: {[key: string]: any} = {}
+  private _gallery: HTMLElement = null;
+  private _lightbox: any = null;
+  private _settings: { [key: string]: any } = {};
+  private _imagesList: { [key: string]: any } = {};
 
   constructor(
     public plugin: ImgGallery,
@@ -18,23 +18,31 @@ export class imgGalleryInit extends MarkdownRenderChild {
     public container: HTMLElement,
     public app: App
   ) {
-    super(container)
+    super(container);
   }
 
   async onload() {
     // parse and normalize settings
-    this._settings = getSettings(this.src, this.container)
-    this._imagesList = getImagesList(this.app, this.container, this._settings)
+    this._settings = getSettings(this.app, this.src, this.container);
+    this._imagesList = getImagesList(this.app, this.container, this._settings);
 
     // inject the pertinent kind of gallery
-    if (this._settings.type === 'horizontal') {
-      this._gallery = buildHorizontal(this.container, this._imagesList, this._settings)
-    } else if (this._settings.type === 'vertical') {
-      this._gallery = buildVertical(this.container, this._imagesList, this._settings)
+    if (this._settings.type === "horizontal") {
+      this._gallery = buildHorizontal(
+        this.container,
+        this._imagesList,
+        this._settings
+      );
+    } else if (this._settings.type === "vertical") {
+      this._gallery = buildVertical(
+        this.container,
+        this._imagesList,
+        this._settings
+      );
     }
 
     // initialize a lightbox
-    this._lightbox = buildLightbox(this._gallery, this._imagesList, this.app)
+    this._lightbox = buildLightbox(this._gallery, this._imagesList, this.app);
   }
 
   async onunload() {
@@ -43,14 +51,14 @@ export class imgGalleryInit extends MarkdownRenderChild {
 
     // destroy the gallery
     if (this._gallery) {
-      this._gallery.remove()
-      this._gallery = null
+      this._gallery.remove();
+      this._gallery = null;
     }
 
     // destroy the lightbox
     if (this._lightbox) {
-      this._lightbox.destroy()
-      this._lightbox = null
+      this._lightbox.destroy();
+      this._lightbox = null;
     }
   }
 }
